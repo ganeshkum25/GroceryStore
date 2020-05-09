70406864977bf2c9b113be08399c3b7686fa9ac1
Ganesh Kumbhar <ganesh.kumbhar@klingelnberg.com>
2020-05-09 11:51:21 +0530

    Adding empty project structure.

---
+++ src/GroceryStore.Boundary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GroceryStore.Sdk;
+
+namespace GroceryStore.Boundary
+{
+    public interface IProductRepository
+    {
+        public Guid Save(Product product);
+
+        public Product Get(Guid productId);
+    }
+}
