fad3e8717bb24f7013d3599856f467c80bc3a59c
Ganesh Kumbhar <ganesh.kumbhar@klingelnberg.com>
2020-05-09 10:49:52 +0530

    Adding empty controller

---
+++ src/GroceryStore.WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GroceryStore.Sdk;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public Category GetCategory(string categoryId)
+        {
+            return null;
+        }
+    }
+}
