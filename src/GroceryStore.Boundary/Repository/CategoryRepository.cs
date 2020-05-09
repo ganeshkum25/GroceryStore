c3daf85ef2399b8af19491b6cbc65426a19888b1
Ganesh Kumbhar <ganesh.kumbhar@klingelnberg.com>
2020-05-09 12:49:11 +0530

    Added repository interfaces

---
+++ src/GroceryStore.Boundary/Repository/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GroceryStore.Sdk;
+
+namespace GroceryStore.Boundary.Repository
+{
+    public interface CategoryRepository
+    {
+        Guid Save(Category product);
+
+        Category Get(Guid productId);
+    }
+}
---
+++ src/GroceryStore.Boundary/Repository/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GroceryStore.Sdk;
+
+namespace GroceryStore.Boundary.Repository
+{
+    public interface ProductRepository
+    {
+        Guid Save(Product product);
+
+        Product Get(Guid productId);
+    }
+}
